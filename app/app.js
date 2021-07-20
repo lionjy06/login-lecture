34f88aa8844005f9ba15c4c268d7eca8fa5a7a35
jin <jyjjyj06@naver.com>
2021-07-20 21:26:59 +0900

    프런트단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set("views", "./src/views"); // view setting
 app.set("view engine", "ejs"); //ejs사용하려면 npm i ejs --save 해줘야 사용가능
-
+app.use(express.static(`${__dirname}/src/public`)); //app.js파일이 있는 디렉토리 경로에 /src/public을 추가해줌
 
 app.use('/', home); // use => 미들웨어를 등록하는 메소드
 
