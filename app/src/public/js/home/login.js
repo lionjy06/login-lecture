26c83d6cf249b06701b00395468c5a18606eac2c
jin <jyjjyj06@naver.com>
2021-07-21 14:21:48 +0900

    돔을 이용하여 html객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hihihi');
+const id = document.querySelector('#id'),
+    pwd = document.querySelector('#pwd'),
+    loginBtn = document.querySelector('button');
+
+console.log(id);
+
+loginBtn.addEventListener('click',login);
+
+function login(){
+  const req = {
+    id : id.value,
+    pwd : pwd.value,
+  } //id에 입력한 값을 받아올수있음 (변수.value), fetch() => 입력받아온 값(id와 pwd값을 서버에 전달해주는 api)
+  console.log(req);
+};
